e4fac03909a407d23bdfc4c0d406fde6e9c053f5
kde713 <kde713@gmail.com>
2017-05-14 07:05:26 +0900

    add dependency files for heroku deploy

--- server.py
+++ server.py
@@ -3,21 +3,28 @@ from unmoapp import views
 
 # Flask init
 application = Flask(__name__)
+allow_origin_url = "https://doorithon-v2-unmo.github.io"
 
 
 @application.route('/member/login', methods=['POST'])
 def member_login():
-    return jsonify(views.member_login(request.form["uid"], request.form["pwd"]))
+    resp = jsonify(views.member_login(request.form["uid"], request.form["pwd"]))
+    resp.headers['Access-Control-Allow-Origin'] = allow_origin_url  # 타 URL 접근 허용
+    return resp
 
 
 @application.route('/member/friends', methods=['POST'])
 def member_friends():
-    return jsonify(views.member_friends(request.form["session_id"]))
+    resp = jsonify(views.member_friends(request.form["session_id"]))
+    resp.headers['Access-Control-Allow-Origin'] = allow_origin_url  # 타 URL 접근 허용
+    return resp
 
 
 @application.route('/service/submit', methods=['POST'])
 def service_submit():
-    return jsonify(views.service_submit(request.get_json()))
+    resp = jsonify(views.service_submit(request.get_json()))
+    resp.headers['Access-Control-Allow-Origin'] = allow_origin_url  # 타 URL 접근 허용
+    return resp
 
 
 # Test Run
