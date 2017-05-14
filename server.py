e58672e4475b2946c63a32d58f9cecf9fada470e
kde713 <kde713@gmail.com>
2017-05-14 12:50:16 +0900

    change CORS module

--- server.py
+++ server.py
@@ -1,37 +1,30 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from unmoapp import views
 
 # Flask init
 application = Flask(__name__)
-allow_origin_url = "https://doorithon-v2-unmo.github.io"
+CORS(application)
 
 
 @application.route('/member/login', methods=['POST'])
 def member_login():
-    resp = jsonify(views.member_login(request.form["uid"], request.form["pwd"]))
-    resp.headers['Access-Control-Allow-Origin'] = allow_origin_url  # 타 URL 접근 허용
-    return resp
+    return jsonify(views.member_login(request.form["uid"], request.form["pwd"]))
 
 
 @application.route('/member/logout', methods=['POST'])
 def member_logout():
-    resp = jsonify(views.member_logout(request.form["session_id"]))
-    resp.headers['Access-Control-Allow-Origin'] = allow_origin_url  # 타 URL 접근 허용
-    return resp
+    return jsonify(views.member_logout(request.form["session_id"]))
 
 
 @application.route('/member/friends', methods=['POST'])
 def member_friends():
-    resp = jsonify(views.member_friends(request.form["session_id"]))
-    resp.headers['Access-Control-Allow-Origin'] = allow_origin_url  # 타 URL 접근 허용
-    return resp
+    return jsonify(views.member_friends(request.form["session_id"]))
 
 
 @application.route('/service/submit', methods=['POST'])
 def service_submit():
-    resp = jsonify(views.service_submit(request.get_json()))
-    resp.headers['Access-Control-Allow-Origin'] = allow_origin_url  # 타 URL 접근 허용
-    return resp
+    return jsonify(views.service_submit(request.get_json()))
 
 
 # Test Run
